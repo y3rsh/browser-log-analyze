0777f317e434115e3537b8376e79be4e669e34ff
Josh McVey <josh.mcvey@rackspace.com>
2019-04-21 08:44:47 -0500

    fix: add logging and pytest settings

--- tests/test_errors.py
+++ tests/test_errors.py
@@ -1,31 +1,30 @@
 import pytest
 import os
 import time
+import logging
+LOGGER = logging.getLogger(__name__)
 
-
-class TestGuineaPig():
-
-    def test_404(self, driver):
-        """
-        validate that 404 errors are caught in logs
-        :return: None
-        """
-        path = f"file:{os.getcwd()}/test_data/network404.html"
-        driver.get(path)
-        browser = driver.get_log('browser')
-        d = driver.get_log('driver')
-        logger = [
-            {
-                'timestamp': int(round(time.time() * 1000)),
-                'url': driver.current_url,
-            }
-        ]
-        for log in browser:
-            if log['level'] == 'SEVERE':
-                print(log)
-                for page in logger:
-                    print(log['timestamp'] - page['timestamp'])
-
-        for log in d:
-            print(log)
+def test_404(driver):
+    """
+    validate that 404 errors are caught in logs
+    :return: None
+    """
+    path = f"file:{os.getcwd()}/test_data/network404.html"
+    driver.get(path)
+    browser = driver.get_log('browser')
+    d = driver.get_log('driver')
+    logger = [
+        {
+            'timestamp': int(round(time.time() * 1000)),
+            'url': driver.current_url,
+        }
+    ]
+    for b_log in browser:
+        if b_log['level'] == 'SEVERE':
+            LOGGER.info(b_log)
+            for page in logger:
+                LOGGER.info((b_log['timestamp'] - page['timestamp']))
+    for log in d:
+        LOGGER.info(log)
 # https://httpbin.org/
+
